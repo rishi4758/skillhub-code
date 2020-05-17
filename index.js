a5bb00f812549bdd63dbc92a19180699826aa27b
Rishav Rapta <rishavrapta79@gmail.com>
2020-05-17 23:21:30 +0530

    klkl

--- index.js
+++ index.js
@@ -24,17 +24,6 @@ secure: false
 app.use(passport.initialize());
 app.use(passport.session());
 app.use(cors({origin:true,credentials: true}));
-app.use(function (req, res, next) {
-  res.header('Access-Control-Allow-Credentials', true);
-  res.header('Access-Control-Allow-Origin', req.headers.origin);
-  res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE');
-  res.header('Access-Control-Allow-Headers', 'Origin, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version, X-Response-Time, X-PINGOTHER, X-CSRF-Token,Authorization');
-  if (req.method === "OPTIONS") {
-      return res.status(200).end();
-  } else {
-      next();
-  }
-});
 
 app.use(bodyParser.json());
 app.use(function(err, req, res, next) {
